a3a66582a268413a238256ad11edfc9eda31781c
Ubuntu <ubuntu@ip-172-31-12-169.us-west-2.compute.internal>
2023-12-12 05:54:48 +0000

    your commit 2

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,7 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/ubuntu/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/usr/local/cuda-12.2/lib64:'
+export LD_LIBRARY_PATH='/home/ubuntu/catkin_ws/devel/lib:/opt/ros/noetic/lib:/usr/local/cuda-12.2/lib64:'
+export PKG_CONFIG_PATH="/home/ubuntu/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/ubuntu/catkin_ws/build'
 export ROSLISP_PACKAGE_DIRECTORIES='/home/ubuntu/catkin_ws/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/ubuntu/catkin_ws/src:$ROS_PACKAGE_PATH"
