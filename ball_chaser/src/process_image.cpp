67bde4ccfc1762c9aa3f490ce10a3e8967795d6e
rsacred <150765199+rsacred@users.noreply.github.com>
2024-03-21 22:39:15 -0500

    Update process_image.cpp

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -25,6 +25,7 @@ void process_image_callback(const sensor_msgs::Image img)
 {
 
     int white_pixel = 255;
+    int pixel_num;
     int pixel_pos;
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
@@ -34,27 +35,32 @@ void process_image_callback(const sensor_msgs::Image img)
 	
     // Loop through each pixel in the image and determine if it's bright white
     for (int i = 0; i < img.height * img.step; i = i + 3) {
-		
-		if (img.data[i] == white_pixel && img.data[i+1] == white_pixel && img.data[i+2] == white_pixel){
+	    
+		if (img.data[i] == white_pixel && img.data[i+1] == white_pixel && img.data[i+2] == white_pixel) {
 			// Determine location of bright white pixel and provide appropriate command
-			pixel_pos = i % img.step;
-			
-			if (pixel_pos <= int(img.step / 3)){
-				ROS_INFO("White ball detected on left");
-				drive_robot(0.5, 0.5);
-				
-			}else if(pixel_pos >= int(img.step * 2/3)){
-				ROS_INFO("White ball detected on right");
-				drive_robot(0.5, -0.5);
-
-			}else{
-				ROS_INFO("White ball detected in middle");	
-				drive_robot(0.5, 0);
-			}
-		}else{
-			drive_robot(0.0, 0.0);
+			pixel_num = i;
+			break;
 		}
-	}
+    }
+    pixel_pos = pixel_num % img.step;	
+    if (pixel_num >= 0) {
+	    if (pixel_pos <= int(img.step / 3)) {
+		    ROS_INFO("White ball detected on left");
+		    drive_robot(0.5, 0.5);
+		
+	    }else if (pixel_pos >= int(img.step * 2/3)) {
+		    ROS_INFO("White ball detected on right");
+		    drive_robot(0.5, -0.5);
+	
+	    }else {
+		    ROS_INFO("White ball detected in middle");	
+		    drive_robot(0.5, 0);
+	    }
+    }
+    else if (pixel_num < 0) {
+	drive_robot(0.0, 0.0);
+}
+	
 }
 
 int main(int argc, char** argv)
